3d928d6663825ecea875df26a8bdc0162d2712cd
Brodalee <boubou-28@live.fr>
2018-10-09 12:46:02 +0200

    Création des 3 types de personnages

---
+++ src/Personnage/Guerrier.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personnage;
+
+/**
+ *
+ * @author alex
+ */
+public class Guerrier extends Personnages{
+    
+    public Guerrier(int level, int strenght, int agility, int intel) {
+        super(level, strenght, agility, intel);
+    }
+    
+}
